60cdea31c510385dd6ebf0d1e37907fad9be8776
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-16 22:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Your Script here.
+function rot13(encodedStr){
+   let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	const string = encodedStr;
+	const arr = [];
+ for(let i = 0;i<string.length;i++){
+	string.charAt(i) = string.charAt(i+13);
+	 arr.push(string[i]);
+ }
